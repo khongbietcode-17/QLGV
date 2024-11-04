abcbdfcbfbcd730acd0b84a51e3dc4a5e6743388
Nhat Tien <ledangnhattien04@gmail.com>
2024-11-04 07:25:25 +0700

    fix: hot fix

--- QLGV/QLGV/Model/GiaoVien.cs
+++ QLGV/QLGV/Model/GiaoVien.cs
@@ -8,5 +8,14 @@ namespace QLGV.Model
 {
     internal class GiaoVien
     {
+        public int Id { get; set; }
+        public string HoLot { get; set; }
+        public string Ten { get; set; }
+        public int GioiTinh { get; set; }
+        public string NgaySinh { get; set; }
+        public string DiaChi { get; set; }
+        public string Email { get; set; }
+        public string SoDienThoai { get; set; }
+        public int BoMonId { get; set; }
     }
 }
--- QLGV/QLGV/formGiaovien.Designer.cs
+++ QLGV/QLGV/formGiaovien.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(217, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Giáo viên";
             // 
             // formGiaovien
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(570, 440);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "formGiaovien";
             this.Text = "formGiaovien";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
